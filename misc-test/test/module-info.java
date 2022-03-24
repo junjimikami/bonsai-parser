87f2b3599e5eaa48dd284ca934221884e52ee6ca
junjimikami <jmikami@unitedjiga.com>
2022-03-24 21:17:14 +0900

    Update module-info.java

--- misc-test/test/module-info.java
+++ misc-test/test/module-info.java
@@ -3,6 +3,8 @@ module com.unitedjiga.commontest.misc {
     requires junit;
     requires org.junit.jupiter.api;
     
-    provides com.unitedjiga.common.parsing.TokenizerFactory with com.unitedjiga.commontest.parsing.sp.TestTokenizerFactory;
+    provides com.unitedjiga.common.parsing.TokenizerFactory
+            with com.unitedjiga.commontest.parsing.sp.TestTokenizerFactory,
+            com.unitedjiga.commontest.parsing.sp.JavaTokenizerFactory;
     provides com.unitedjiga.common.parsing.ParserFactory with com.unitedjiga.commontest.parsing.sp.TestParserFactory;
 }
