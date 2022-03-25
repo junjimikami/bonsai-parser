0b6ea1a61db6be462066d687c42e355c17988b96
junjimikami <jmikami@unitedjiga.com>
2022-03-26 00:51:08 +0900

    Add file for testing

---
+++ misc-test/test/com/unitedjiga/commontest/parsing/sp/JavaParserFacotry.java
@@ -0,0 +1,15 @@
+package com.unitedjiga.commontest.parsing.sp;
+
+import com.unitedjiga.common.parsing.Parser;
+import com.unitedjiga.common.parsing.ParserFactory;
+import com.unitedjiga.common.parsing.Tokenizer;
+
+public class JavaParserFacotry implements ParserFactory {
+
+    @Override
+    public Parser createParser(Tokenizer tokenizer) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
