b694ba9afe78d73b31c1ea438d93feeda15a42a6
junjimikami <jmikami@jiganaut.com>
2024-10-12 22:25:12 +0900

    Fix implementation.

--- api/src/main/java/com/jiganaut/bonsai/grammar/impl/AbstractCompositeRule.java
+++ api/src/main/java/com/jiganaut/bonsai/grammar/impl/AbstractCompositeRule.java
@@ -5,33 +5,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
-import com.jiganaut.bonsai.grammar.Quantifiable;
-import com.jiganaut.bonsai.grammar.QuantifierRule;
 import com.jiganaut.bonsai.grammar.Rule;
 
 abstract class AbstractCompositeRule extends AbstractRule implements DefaultQuantifiableRule {
-    static abstract class Builder extends AbstractRule.Builder implements Quantifiable {
+    static abstract class Builder extends AbstractRule.Builder implements DefaultQuantifiableRule.Builder {
         protected final List<Supplier<Rule>> builders = new ArrayList<>();
-
-        @Override
-        public QuantifierRule atLeast(int times) {
-            if (times < 0) {
-                throw new IllegalArgumentException(Message.NEGATIVE_PARAMETER.format());
-            }
-            return new DefaultQuantifierRule(build(), times);
-        }
-
-        @Override
-        public QuantifierRule range(int from, int to) {
-            if (from < 0) {
-                throw new IllegalArgumentException(Message.NEGATIVE_PARAMETER.format());
-            }
-            if (to < from) {
-                throw new IllegalArgumentException(Message.INVALID_MAX_COUNT.format());
-            }
-            return new DefaultQuantifierRule(build(), from, to);
-        }
-
     }
 
     protected final List<Rule> elements;
--- api/src/main/java/com/jiganaut/bonsai/grammar/impl/DefaultQuantifiableRule.java
+++ api/src/main/java/com/jiganaut/bonsai/grammar/impl/DefaultQuantifiableRule.java
@@ -10,22 +10,43 @@ import com.jiganaut.bonsai.grammar.Rule;
  */
 interface DefaultQuantifiableRule extends Rule, Quantifiable {
 
+    interface Builder extends Rule.Builder, Quantifiable {
+
+        @Override
+        public default QuantifierRule atLeast(int times) {
+            checkParameter(times);
+            return new DefaultQuantifierRule(build(), times);
+        }
+
+        @Override
+        public default QuantifierRule range(int from, int to) {
+            checkParameter(from, to);
+            return new DefaultQuantifierRule(build(), from, to);
+        }
+    }
+
     @Override
     public default QuantifierRule atLeast(int times) {
-        if (times < 0) {
-            throw new IllegalArgumentException(Message.NEGATIVE_PARAMETER.format());
-        }
+        checkParameter(times);
         return new DefaultQuantifierRule(this, times);
     }
 
     @Override
     public default QuantifierRule range(int from, int to) {
-        if (from < 0) {
+        checkParameter(from, to);
+        return new DefaultQuantifierRule(this, from, to);
+    }
+
+    private static void checkParameter(int times) {
+        if (times < 0) {
             throw new IllegalArgumentException(Message.NEGATIVE_PARAMETER.format());
         }
+    }
+
+    private static void checkParameter(int from, int to) {
+        checkParameter(from);
         if (to < from) {
             throw new IllegalArgumentException(Message.INVALID_MAX_COUNT.format());
         }
-        return new DefaultQuantifierRule(this, from, to);
     }
 }
