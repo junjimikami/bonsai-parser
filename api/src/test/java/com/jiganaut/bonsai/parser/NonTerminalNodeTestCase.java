6aa34ea81f84e12aad330848bc29180bdb4226a6
junjimikami <jmikami@jiganaut.com>
2024-12-09 23:20:08 +0900

    Fixed test cases.

--- api/src/test/java/com/jiganaut/bonsai/parser/NonTerminalNodeTestCase.java
+++ api/src/test/java/com/jiganaut/bonsai/parser/NonTerminalNodeTestCase.java
@@ -1,7 +1,13 @@
 package com.jiganaut.bonsai.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,6 +52,54 @@ interface NonTerminalNodeTestCase extends TreeTestCase {
 
             var ex = assertThrows(NullPointerException.class, () -> builder.build());
             testReporter.publishEntry(ex.getMessage());
+        }
+
+        @SuppressWarnings("exports")
+        @Test
+        @DisplayName("add(Tree) [Null parameter]")
+        default void addInCaseOfNullParameter(TestReporter testReporter) throws Exception {
+            assumeFalse(isSetName());
+
+            var builder = createTarget();
+
+            var ex = assertThrows(NullPointerException.class, () -> builder.add(null));
+            testReporter.publishEntry(ex.getMessage());
+        }
+
+        @Test
+        @DisplayName("add(Tree)")
+        default void add() throws Exception {
+            assumeTrue(canBuild());
+
+            var builder = createTarget();
+
+            assertEquals(builder, builder.add(mock(Tree.class)));
+        }
+
+        @SuppressWarnings("exports")
+        @Test
+        @DisplayName("addAll(NonTerminalNode.Builder) [Null parameter]")
+        default void addAllInCaseOfNullParameter(TestReporter testReporter) throws Exception {
+            assumeFalse(isSetName());
+
+            var builder = createTarget();
+
+            var ex = assertThrows(NullPointerException.class, () -> builder.addAll(null));
+            testReporter.publishEntry(ex.getMessage());
+        }
+
+        @Test
+        @DisplayName("addAll(NonTerminalNode.Builder)")
+        default void addAll() throws Exception {
+            assumeTrue(canBuild());
+
+            var builder = createTarget();
+
+            var mockNode = mock(NonTerminalNode.class);
+            when(mockNode.getSubTrees()).thenReturn(List.of());
+            var mockBuilder = mock(NonTerminalNode.Builder.class);
+            when(mockBuilder.build()).thenReturn(mockNode);
+            assertEquals(builder, builder.addAll(mockBuilder));
         }
 
     }
