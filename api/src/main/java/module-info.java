a6f824727ab7dffbf4a0eb9f4f3afaba21f965b4
junjimikami <jmikami@jiganaut.com>
2026-06-14 17:07:45 +0900

    Remove unused TokenizerFactory and ParserFactory usages from module-info.java

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -2,7 +2,5 @@ module com.jiganaut.bonsai {
     exports com.jiganaut.bonsai.parser;
     exports com.jiganaut.bonsai.grammar;
 
-    uses com.jiganaut.bonsai.parser.TokenizerFactory;
-    uses com.jiganaut.bonsai.parser.ParserFactory;
     uses com.jiganaut.bonsai.parser.spi.ParserProvider;
 }
